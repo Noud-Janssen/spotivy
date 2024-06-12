90e5c6700eb7b77483f797a81caa3ad9440a005d
Noud <noudjanssen123@gmail.com>
2024-06-12 12:13:51 +0200

    eod

---
+++ src/model/queue.cpp
@@ -0,0 +1,15 @@
+#include "model/queue.h"
+
+namespace model
+{
+    queue::queue()
+        : song_list_base("queue")
+    {
+
+    }
+
+    void queue::next()
+    {
+        
+    }
+} // namespace model
