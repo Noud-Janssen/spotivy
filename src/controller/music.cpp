33c234400e15a98323d6902da4b21377cb372f48
Noud <noudjanssen123@gmail.com>
2024-06-12 11:18:02 +0200

    Added list feature, added some example artist with albums and songs

--- src/controller/music.cpp
+++ src/controller/music.cpp
@@ -17,4 +17,16 @@ namespace controller
     std::vector<model::artist> music::get_artists() {
         return model::artist::all();
     }
+
+    model::artist music::find_artist(std::string s)
+    {
+        std::vector<model::artist> artists = get_artists();
+        for (model::artist art : artists) {
+            if (art.get_name() == s) {
+                return art;
+            }
+        }
+        return model::artist(s);
+
+    }
 } // namespace controller
