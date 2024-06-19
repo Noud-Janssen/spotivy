63ac5f8686b0cac44d41112ec1965760e1d88ed1
Noud <noudjanssen123@gmail.com>
2024-06-19 10:38:32 +0200

    Added documentation

--- test/model/album.cpp
+++ test/model/album.cpp
@@ -12,10 +12,14 @@ TEST(model_album, constructor) {
         model::song("Artist1","Album1","Song5"),
         model::song("Artist1","Album1","Song6"),
     });
+
     EXPECT_TRUE(album[0].get_song() == "Song1");
     EXPECT_TRUE(album[1].get_song() == "Song2");
     EXPECT_TRUE(album[2].get_song() == "Song3");
     EXPECT_TRUE(album[3].get_song() == "Song4");
     EXPECT_TRUE(album[4].get_song() == "Song5");
     EXPECT_TRUE(album[5].get_song() == "Song6");
+    
+    EXPECT_TRUE(album.get_title() == "Album1");
+    EXPECT_TRUE(album.get_size() == 6);
 }
